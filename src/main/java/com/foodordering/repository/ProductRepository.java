d9e81a2affa99cbc2d18350b5b02411af9c6cd66
EmiyaKurou001 <vudangkhoa922308@gmail.com>
2025-11-01 09:17:44 +0700

    Add more entity

--- src/main/java/com/foodordering/repository/ProductRepository.java
+++ src/main/java/com/foodordering/repository/ProductRepository.java
@@ -1,17 +1,3 @@
 package com.foodordering.repository;
 
-import com.foodordering.model.entity.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.util.List;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-    // Find all non-deleted products using property name
-    List<Product> findByIsDeletedFalse();
-
-    // Explicit MongoDB query by stored field name
-    @Query("{ 'is_deleted' : false }")
-    List<Product> findAllActive();
-}
+// ProductRepository removed intentionally. Remove file from git if you want to delete permanently.
